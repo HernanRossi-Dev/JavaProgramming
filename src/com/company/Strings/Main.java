2a5babe72e875cfe7cd2c7579d026d6ec3d20971
HernanRossi <hernan_rossi@msn.com>
2016-05-01 13:29:05 -0700

    Initial commit

---
+++ src/com/company/Strings/Main.java
@@ -0,0 +1,49 @@
+package com.company.Strings;
+
+import com.company.LinkedLists.LinkedListNode;
+import com.company.LinkedLists.TwoPointEight;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //OnePointOne questionOne = new OnePointOne(test);
+        //questionOne.run();
+
+        //OnePointTwo q2 = new OnePointTwo();
+       //System.out.println("Question two is :" +  q2.test());
+       // OnePointThree q3 = new OnePointThree();
+        //q3.run();
+        //System.out.println(q3.run2());
+        //OnePointFour q4 = new OnePointFour();
+        //System.out.println("The result was : " + q4.run());
+        //OnePointFive q5 = new OnePointFive();
+        //out("The result was : " + q5.run());
+        //OnePointSix q16 = new OnePointSix();
+        //out("The result was : " + q16.run());
+        /*OnePointSeven q17 = new OnePointSeven();
+        int[][] input = new int[][]{
+                {1,2,3,4},
+                {1,2,3,4},
+                {1,2,3,4},
+                {1,2,3,4},};
+        q17.run(input);*/
+       /* OnePointEight q8 = new OnePointEight();
+        int[][] input = new int[][] {
+                {1,2,3,4,5},
+                {1,2,3,0,5},
+                {1,2,0,4,5},
+                {1,2,3,4,5},};
+        q8.run(input);*/
+        //OnePointNine q9 = new OnePointNine();
+        //out("The out put of one point nine is: " + q9.run());
+
+    }
+
+    public static void out(String out) {
+        System.out.println(out);
+    }
+
+
+}
