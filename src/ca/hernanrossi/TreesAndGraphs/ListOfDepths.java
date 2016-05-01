24f6d990c88215aa134c088c13519bf205b2b03f
HernanRossi <hernan_rossi@msn.com>
2016-05-01 14:27:49 -0700

    Initial commit

---
+++ src/ca/hernanrossi/TreesAndGraphs/ListOfDepths.java
@@ -0,0 +1,14 @@
+package ca.hernanrossi.TreesAndGraphs;
+
+/**********************************************************************************************************************
+ * Created by Hernan Rossi
+ * Problem Description:
+ *              Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth(e.g.
+ *                  if you have a tree with depth D, you'll have D linked lists:
+ *Solution method:
+ *              Traversing the binary tree in a depth first search manner and creating a new linked lilst for every
+ *                  2^n nodes ( a new linked list for every level in the tree) then well will have a linked list for
+ *                  every level in the tree.
+ *********************************************************************************************************************/
+public class ListOfDepths {
+}
